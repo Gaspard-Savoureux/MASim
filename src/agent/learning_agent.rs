0fbb3012dda8479ef187450274f0fa7c5a748558
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-02-06 19:31:41 -0500

    updated learning agent

--- src/agent/learning_agent.rs
+++ src/agent/learning_agent.rs
@@ -1,81 +1,110 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, fmt::Debug, hash::Hash, rc::Rc};
 
-use macroquad::{math::UVec2, rand::ChooseRandom};
+use macroquad::rand::ChooseRandom;
 
-pub enum AgentType {}
+use crate::{environment::environment::Env, scheduler::scheduler::Position};
 
-#[derive(PartialEq, Eq, Hash)]
+use super::state::State;
+
+pub type Done = bool;
+pub type StepFunction = Rc<
+    dyn Fn(&LearningAgent, &Env, Position, &State, &'static str) -> (Position, State, Reward, Done),
+>;
+pub type Reward = f32;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Q {
-    state: UVec2,
+    state: State,
     action: &'static str,
 }
 
+#[derive()]
 pub struct LearningAgent {
-    /// unique if of the agent
-    id: u32,
+    /// unique id of the agent
+    pub id: u32,
+    /// The type of the agent. Example: wolf, sheep, etc.
+    pub agent_type: &'static str,
     /// position in a 2D graphe
-    position: UVec2,
-    /// reward for a given state and action
-    // q_values: HashMap<u32, f32>,
+    pub state: State,
+    /// Q-values
     q_values: HashMap<Q, f32>,
-    /// learning rate
-    alpha: f32,
-    /// discount factor
-    gamma: f32,
-    /// exploration rate
-    epsilon: f32,
+    /// alpha / learning rate
+    learning_rate: f32,
+    /// gamma / discount factor
+    discount_factor: f32,
+    /// epsilon / exploration rate
+    exploration_rate: f32,
+    /// Function representing a step
+    step_fn: StepFunction,
 }
 
 impl LearningAgent {
     pub fn new(
         id: u32,
-        position: UVec2,
-        alpha: Option<f32>,
-        gamma: Option<f32>,
-        epsilon: Option<f32>,
+        agent_type: &'static str,
+        state: State,
+        learning_rate: Option<f32>,
+        discount_factor: Option<f32>,
+        exploration_rate: Option<f32>,
+        step_fn: &StepFunction,
     ) -> LearningAgent {
-        let alpha = alpha.unwrap_or(0.1);
-        let gamma = gamma.unwrap_or(0.9);
-        let epsilon = epsilon.unwrap_or(0.2);
+        let learning_rate = learning_rate.unwrap_or(0.1);
+        let discount_factor = discount_factor.unwrap_or(0.9);
+        let exploration_rate = exploration_rate.unwrap_or(0.2);
 
         LearningAgent {
             id,
-            position,
+            agent_type,
+            state,
             q_values: HashMap::new(),
-            alpha,
-            gamma,
-            epsilon,
+            learning_rate,
+            discount_factor,
+            exploration_rate,
+            step_fn: Rc::clone(step_fn),
         }
     }
 
-    pub fn get_q_value(&self, state: UVec2, action: &'static str) -> &f32 {
+    pub fn get_q_value(&self, state: State, action: &'static str) -> &f32 {
         let k = Q { state, action };
 
         match self.q_values.get(&k) {
             Some(value) => value,
-            None => panic!("get_q_value(...): no value found"),
+            None => &0.,
         }
     }
 
-    pub fn set_q_value(&mut self, state: UVec2, action: &'static str, value: f32) {
+    pub fn set_q_value(&mut self, state: State, action: &'static str, value: f32) {
         self.q_values.insert(Q { state, action }, value);
     }
 
-    pub fn move_to(&mut self, position: UVec2) {
-        self.position = position;
+    pub fn update_state(&mut self, state: State) {
+        self.state = state;
+    }
+
+    /// Returns subset of q values with the same state and actions
+    fn q_values_subset(&self, state: &State, actions: &Vec<&'static str>) -> HashMap<&Q, &f32> {
+        self.q_values
+            .iter()
+            .filter(|(k, _)| k.state == *state && actions.contains(&k.action))
+            .collect()
+    }
+
+    fn max_q_val(&self, q_values: &HashMap<&Q, &f32>) -> f32 {
+        let max_entry = q_values.iter().max_by(|&a, &b| a.1.total_cmp(b.1));
+
+        match max_entry {
+            Some((_, val)) => **val,
+            None => 0.0,
+        }
     }
 
     /// Epsilon-greedy action selection
-    pub fn choose_action(&self, state: UVec2, actions: &Vec<&'static str>) -> &'static str {
-        if rand::random_range(0.0..1.) < self.epsilon {
+    pub fn choose_action(&self, state: &State, actions: &Vec<&'static str>) -> &'static str {
+        if rand::random_range(0.0..1.) < self.exploration_rate || self.q_values.is_empty() {
             return actions.choose().unwrap();
         }
 
-        let q_values: HashMap<&Q, &f32> = self
-            .q_values
-            .iter()
-            .filter(|(k, _)| k.state == state && actions.contains(&k.action))
-            .collect();
+        let q_values = self.q_values_subset(state, actions);
 
         let max_entry = q_values.iter().max_by(|a, b| a.1.total_cmp(b.1));
 
@@ -90,39 +119,101 @@ impl LearningAgent {
 
         ""
     }
+
+    /// Q-learning update
+    ///
+    /// Q-learning update rule:
+    /// Q(s, a) <- Q(s, a) + alpha * (reward + gamma * max_a' Q(s', a') - Q(s, a))
+    pub fn update(
+        &mut self,
+        state: &State,
+        action: &'static str,
+        reward: f32,
+        next_state: &State,
+        next_actions: &Vec<&'static str>,
+    ) {
+        let old_q_value = self.get_q_value(state.clone(), action);
+        let future_q_value = self.max_q_val(&self.q_values_subset(next_state, next_actions));
+
+        // Q-learning update
+        let new_q_value = old_q_value
+            + self.learning_rate * (reward + self.discount_factor * future_q_value - old_q_value);
+        self.set_q_value(state.clone(), action, new_q_value);
+    }
+
+    pub fn step(
+        &self,
+        env: &Env,
+        position: Position,
+        state: &State,
+        action: &'static str,
+    ) -> (Position, State, Reward, Done) {
+        (self.step_fn)(&self, env, position, state, action)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::agent::state::Value;
+
     use super::*;
 
     #[test]
     fn choosing_action() {
-        let mut agent = LearningAgent::new(0, UVec2 { x: 0, y: 0 }, None, None, Some(0.));
+        let agent_type = "wolf";
+        let func: StepFunction = Rc::new(
+            move |_agent: &LearningAgent,
+                  _env: &Env,
+                  _position: Position,
+                  _state: &State,
+                  _action: &'static str|
+                  -> (Position, State, Reward, Done) {
+                (Position { x: 0, y: 0 }, vec![Value::VI32(32)], 0., true)
+            },
+        );
+        // example: [energy, day_lived, bald]
+        let default_state = vec![Value::VI32(4), Value::VU32(23456), Value::VBool(false)];
+
+        let mut agent = LearningAgent::new(
+            0,
+            agent_type,
+            default_state.clone(),
+            None,
+            None,
+            Some(0.),
+            &func,
+        );
+
         let actions = vec!["eat", "move", "dance", "sing"];
 
-        assert_eq!(agent.choose_action(UVec2 { x: 0, y: 0 }, &actions), "");
+        // assert_eq!(agent.choose_action(default_state, &actions), "");
 
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "eat", 0.);
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "move", 1.);
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "dance", 2.);
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "sing", 3.);
+        agent.set_q_value(default_state.clone(), "eat", 0.);
+        agent.set_q_value(default_state.clone(), "move", 1.);
+        agent.set_q_value(default_state.clone(), "dance", 2.);
+        agent.set_q_value(default_state.clone(), "sing", 3.);
 
-        assert_eq!(agent.choose_action(UVec2 { x: 0, y: 0 }, &actions), "sing");
-        assert_ne!(agent.choose_action(UVec2 { x: 1, y: 0 }, &actions), "sing");
+        assert_eq!(agent.choose_action(&default_state, &actions), "sing");
+        assert_ne!(
+            agent.choose_action(&vec![Value::VBool(true)], &actions),
+            "sing"
+        );
 
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "eat", 4.);
+        agent.set_q_value(vec![Value::VBool(true)], "eat", 4.);
 
-        assert_eq!(agent.choose_action(UVec2 { x: 0, y: 0 }, &actions), "eat");
-        assert_ne!(agent.choose_action(UVec2 { x: 1, y: 0 }, &actions), "eat");
+        assert_eq!(agent.choose_action(&default_state, &actions), "eat");
+        assert_ne!(
+            agent.choose_action(&vec![Value::VBool(false)], &actions),
+            "eat"
+        );
 
-        agent.set_q_value(UVec2 { x: 0, y: 0 }, "move", 4.);
+        agent.set_q_value(vec![Value::VBool(false)], "move", 4.);
 
         let mut count_eat = 0;
         let mut count_move = 0;
 
         for _ in 0..1000 {
-            let result = agent.choose_action(UVec2 { x: 0, y: 0 }, &actions);
+            let result = agent.choose_action(&default_state, &actions);
 
             match result {
                 "eat" => count_eat += 1,
