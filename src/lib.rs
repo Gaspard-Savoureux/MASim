f5b2025486a8983116054d35d038b829153b8421
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-02-12 13:10:00 -0500

    simplified action definition

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 /// For example:
 /// The following:
 /// ```rust
-/// define_actions!(EAT: 0, SING: 1, DANCE: 2)
+/// define_actions!(EAT, SING, DANCE)
 /// ```
 /// Is equivalent to:
 /// ```rust
@@ -12,11 +12,23 @@
 /// pub const DANCE: u32 = 2;
 /// pub static ACTIONS: &[32] = &[EAT, SING, DANCE];
 /// ```
+///
+/// NOTE: If interested this was a pretty good read for incremental TT munchers: https://danielkeep.github.io/tlborm/book/pat-incremental-tt-munchers.html
 #[macro_export]
 macro_rules! define_actions {
-    ($($action_name:ident: $value:expr),+) => {
-        $(pub const $action_name: u32 = $value;)+
-        pub static ACTIONS: &[u32] = &[$($action_name),*];
+    // end of recursion
+    ($i:expr ; ) => {};
+
+    // action name remaining
+    ($i:expr ; $action_name:ident $(, $tail:ident)*) => {
+        pub const $action_name: u32 = $i;
+        define_actions!($i + 1; $($tail),*);
+    };
+
+    // entry point
+    ( $($action_name:ident),+ ) => {
+        define_actions!(0; $($action_name),*);
+        pub static ACTIONS: &[u32] = &[$($action_name),+];
     };
 }
 
@@ -25,7 +37,7 @@ mod tests {
 
     #[test]
     fn test_define_actions() {
-        define_actions!(EAT: 0, SING: 1, DANCE: 2);
+        define_actions!(EAT, SING, DANCE);
 
         assert_eq!(EAT, 0);
         assert_eq!(SING, 1);
